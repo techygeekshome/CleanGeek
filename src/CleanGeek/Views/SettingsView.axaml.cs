cc135708df8598cb651d33e25727c6805babc87c
TechyGeeksHome | Andrew Armstrong <41306705+techygeekshome@users.noreply.github.com>
2026-08-28 09:59:35 +0100

    Add the Settings screen

---
+++ src/CleanGeek/Views/SettingsView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CleanGeek.Views;
+
+public partial class SettingsView : UserControl
+{
+    public SettingsView() => AvaloniaXamlLoader.Load(this);
+}
